8f08d0a5fb1f3c2e339d852f056e6cf829ecd7eb
Vantana <geroyvania@gmail.com>
2025-01-12 18:27:11 +0100

    Обновление файлов

---
+++ day 8-9/bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    n--;
+  } while (swapped);
+
+  return arr;
+}
+
+console.log(bubbleSort([5, 3, 6, 9, 2, 4, 1]));
